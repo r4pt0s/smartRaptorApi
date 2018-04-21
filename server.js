edd33c7278fd0be2f3c42e128bc5fbe05e8c402d
r4pt0s <r4pt0s@protonmail.com>
2018-04-21 13:43:05 +0200

    updating database credentials

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const cors= require('cors');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'r4pt0s',
-    password : 'test123',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+  	ssl: true,
   }
 });
 
