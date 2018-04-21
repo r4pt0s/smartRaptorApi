eb0a7c4b2202921b8ed64166747f354ac469d1a2
r4pt0s <r4pt0s@protonmail.com>
2018-04-21 13:52:09 +0200

    clarifai api key fix

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai= require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'YOUR_CLARIFAI_API_KEY'
+ apiKey: 'cd9b0f70fdcc4dacbd647150626de474'
 });
 
 const handleApiCall= (req,res) =>{
